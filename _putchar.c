e01567b81c481c0ae9e660b2b1b6c078a2bb56dc
EREEK71 <ericelesho@gmail.com>
2022-07-11 02:11:43 -0700

    group

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _putchar - prints out a character
+ * @c: character to be printed out
+ *
+ * Return: Always Success (0)
+ */
+
 int _putchar(char c)
 {
 	write(1,&c, 1);
